21723017a1d239ebf011fb9e698adb7dfb2811b5
Ny777777 <tristan890620@gmail.com>
2026-09-17 14:02:08 +0800

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,63 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        '''
+        1. sorted every charaters in the list
+        2. append the word in the inner list
+        3. if the charaters already exsit, append that word in that inner list
+        '''
+     
+        # #　sorted 
+        # sorted_strs =[]
+        # for i in range(len(strs)):
+        #     s = sorted((strs[i]))
+        #     ss = ''.join(s)
+        #     sorted_strs.append(ss)
+        #     # print(ss)
+        #     # print(sorted_strs)
+     
+
+        # print(sorted_strs)
+
+        # # append the word in the inner list
+        # # 
+        # ans = []
+        # for t in  range(len(sorted_strs)):
+        #     inner_list = []
+        #     if any(strs[t] in j for j in ans):
+        #         continue
+        #     else:
+        #         inner_list.append(strs[t])
+            
+        #     for i in range(len(sorted_strs)):
+        #         if t == i:
+        #             continue 
+        #         if sorted_strs[t] == sorted_strs[i]:
+        #             inner_list.append(strs[i])
+
+        #     ans.append(inner_list)
+        #     print(inner_list)
+        # return ans
+
+        group = {} # slove this question by using dictionary
+
+        for words in strs:
+            key = ''.join(sorted(words))
+            if key in group:
+                group[key].append(words)
+            else:
+                # add new key and this word in this dic at the same time.
+                group[key] = [words] 
+ 
+        ans = []
+
+        for key in group:
+            ans.append(group[key])
+        return ans
+
+        print(ans)
+
+            
+
+
+
+                
